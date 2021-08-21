c98d0f7dce9fec3d48afff3e47bad2bc4036994a
Amanda Maria <amanda.moov@gmail.com>
2021-08-21 12:01:07 -0300

    Atribuindo comportamento a class. Criando métodos

--- index.js
+++ index.js
@@ -2,23 +2,39 @@ class Cliente {
 //a classe contem atributos ou propriedades ou campos. Similares a variáveis.
     nome;
     cpf;
+}
+
+class ContaCorrente{
     agencia;
     saldo;
 
+    //método em classe similar a funçao
+    sacar(valor) {
+        if(this.saldo >= valor){
+            this.saldo -= valor;
+        }
+    } 
 }
 
-const cliente1 = new Cliente(); //novo objeto. Instanciando um novo objeto. 
-const cliente2 = new Cliente();
 
+const cliente1 = new Cliente(); //novo objeto. Instanciando um novo objeto. 
 cliente1.cpf = 111222333444;
 cliente1.nome = "Ricardo";
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 
+
+const cliente2 = new Cliente()
 cliente2.cpf = 333222111000;
 cliente2.nome = "Alice";
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
 
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.saldo = 0;
+contaCorrenteRicardo.agencia = 1001;
+
+console.log(contaCorrenteRicardo.saldo);
+contaCorrenteRicardo.saldo = 100;
+console.log(contaCorrenteRicardo.saldo);
+contaCorrenteRicardo.sacar(50);
+
+console.log(contaCorrenteRicardo.saldo);
 console.log(cliente1);
 console.log(cliente2);
