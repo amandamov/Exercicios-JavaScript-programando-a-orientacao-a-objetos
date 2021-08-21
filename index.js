1dd42e00ec75934162da5db3fa8354187bae0442
Amanda Maria <amanda.moov@gmail.com>
2021-08-21 13:05:37 -0300

    Adicionando return e early return no método

--- index.js
+++ index.js
@@ -1,32 +1,30 @@
 class Cliente {
-    //a classe contem atributos ou propriedades ou campos. Similares a variáveis.
         nome;
         cpf;
     }
     
     class ContaCorrente{
         agencia;
-        _saldo = 0; //se usar #, ele realmente vai ficar prvado. Porém, é uma má prática. Entao, por convençao, utilizamos apenas o _ para indicar que é um atributo privado e que nao deve ser mexido fora da class. 
-    
-        //método em classe similar a funçao. Recebe um parametro ou argumento, nesse caso, "valor".
+        _saldo = 0; 
+       
         sacar(valor) {
             if(this._saldo >= valor){
                 this._saldo -= valor;
+                return valor;
             }
         } 
     
         depositar(valor) {
-            if(valor > 0){
-                this._saldo += valor;
-                console.log(this._saldo);
-            }
+            if(valor <= 0){
+                return;
+            } 
+            this._saldo += valor;
         }
     }
     
-    const cliente1 = new Cliente(); //novo objeto. Instanciando um novo objeto. 
+    const cliente1 = new Cliente();
     cliente1.cpf = 111222333444;
     cliente1.nome = "Ricardo";
-    
     
     const cliente2 = new Cliente()
     cliente2.cpf = 333222111000;
@@ -39,8 +37,8 @@ class Cliente {
     contaCorrenteRicardo.depositar(100);
     contaCorrenteRicardo.depositar(100);
 
-    contaCorrenteRicardo.sacar(50);
-    //contaCorrenteRicardo.depositar(-1); //método com condiçao funciona. 
+    const valorSacado = contaCorrenteRicardo.sacar(50);
+    console.log(valorSacado);
     
     console.log(contaCorrenteRicardo);
      
